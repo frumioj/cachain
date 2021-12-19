f2a27c3d5f18c56f5de48694fde076c0fdf61754
John Kemp <stable.pseudonym@gmail.com>
2021-12-19 09:21:51 -0500

    adding intermediate CA

--- create-chain.sh
+++ create-chain.sh
@@ -28,3 +28,30 @@ while read -ep "Path for root CA [${PWD}/rootca]:" rootcadir </dev/tty; do
     fi 
 done
 
+while read -ep "Path for INTERMEDIATE CA [${PWD}/interca]:" intercadir </dev/tty; do
+    intercadir=${intercadir:-${PWD}/interca}
+    if [ -d "${intercadir}" ]; then
+         echo "${file_dir} already exists - please enter a valid directory path."
+    else
+	echo "Creating... $intercadir"
+        mkdir -p "${intercadir}"
+	mkdir -p "${intercadir}/certs"
+	mkdir -p "${intercadir}/crl"
+	mkdir -p "${intercadir}/newcerts"
+	mkdir -p "${intercadir}/private"
+	chmod 700 "${intercadir}/private"
+	touch "${intercadir}/index.txt"
+	echo 1000 > "${intercadir}/serial"
+	# substitute the $intercadir value into the template file
+	envsubst < inter_openssl.cnf > ${intercadir}/openssl.cnf
+	echo "Generating a 4096 bit RSA key for CA"
+	openssl genrsa -aes256 -out "${intercadir}/private/ca.key.pem" 4096
+
+	# sign a cert with the ROOT key
+	openssl req -config ${rootcadir}/openssl.cnf \
+		-key ${intercadir}/private/ca.key.pem \
+		-new -x509 -days 7300 -sha256 -extensions v3_ca \
+		-out ${intercadir}/certs/ca.cert.pem
+        break
+    fi 
+done
